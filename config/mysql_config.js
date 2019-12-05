bfd67bb74aee5fd30d3e5f9c308bf230851cf6cf
zhongjian <gzzhongjian@yonyou.com>
2019-12-05 15:52:06 +0800

    test

--- config/mysql_config.js
+++ config/mysql_config.js
@@ -4,7 +4,7 @@ const mysql_config = {
   host: 'localhost',
   user: 'root',
   password: "123456",
-  socketPath: '/tmp/mysql.sock.lock'
+  socketPath: '/tmp/mysql.sock'
 }
 
 const createPool = mysql.createPool(mysql_config)
